a8e4ed7463869080834916fc27841c05b4d6ecf7
GERMAN <GERMANALEXIS231997.GAAC@GMAIL.COM>
2019-07-09 19:49:35 -0600

    modified incidentes

--- routes/api/incidentes.js
+++ routes/api/incidentes.js
@@ -15,17 +15,47 @@ function initIncidentes(db){
     });
     router.get('/:id', (req, res, next)=>{ 
         var id = new ObjectID(req.params.id);
-        incidentescoll.findOne({"_id":id},(err,doc))=>{
+        incidentescoll.findOne({"_id":id},(err,doc)=>{
             if (err){  
                  console.log(err);
                  return res.status(404).json({"error":"no se puede obtener los registros"})
             
                 }
 
-            return res.status(200).json(doc)
+            return res.status(200).json(doc);
 
-        }
+        });
     }); 
+    router.post('/',(req, res, next)=>{
+        var newIncidentes = Object.assign(
+            {},
+            {
+                "descripcion":"",
+                "fechaYHora": new date().getTime(),
+                "tipo":"", 
+                "estado" :"",
+                "usuarioRegistra":"",
+                "usuarioAsignado":0,
+                "fechaHoraAsignado": new date(),
+                "fechaHoraCerrado": new date()
+            },
+            req.body 
+            );
+            incidentescoll.insertOne(newIncidentes,(err,rslt)=>{
+                if(err){
+                    console.log(err);
+                    return res.status(404).json({"error":"no se puede agregar"});
+                }
+
+                if(rslt.ops.length==0){
+                    console.log(rslt);
+                    return res.status(200).json({"error":"no se puede agregar"});
+                }
+                return res.status(200).json(rslt.ops[0]);
+            })
+
+    });
+
     return router;
 }
 
