efc4b5a149b96134a7f3c490e6a84fb580139f52
GERMAN <GERMANALEXIS231997.GAAC@GMAIL.COM>
2019-07-09 19:48:03 -0600

    modifies incidentes

--- routes/api/incidentes.js
+++ routes/api/incidentes.js
@@ -1,11 +1,31 @@
 var express = require ('express');
 var router = express.Router();
-
+var ObjectID= require('mongodb').ObjectID;
+ 
 function initIncidentes(db){
     var incidentescoll = db.collection('plantas');
     router.get('/',(req, res, next)=>{
-        res.status(200).json({"api":"1"}); 
+        incidentescoll.find().toArray((err,incidentes)=>{
+            if(err){
+                console.log(err);
+                return res.status(404).json({"error":"error"})
+            };
+            return res.status(200).json(incidentes)
+        });
     });
+    router.get('/:id', (req, res, next)=>{ 
+        var id = new ObjectID(req.params.id);
+        incidentescoll.findOne({"_id":id},(err,doc))=>{
+            if (err){  
+                 console.log(err);
+                 return res.status(404).json({"error":"no se puede obtener los registros"})
+            
+                }
+
+            return res.status(200).json(doc)
+
+        }
+    }); 
     return router;
 }
 
